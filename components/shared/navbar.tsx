a726cdc6f8490d9ea4d3209d0a6ab224d0ea0205
NeilRow56 <neilrowland56@gmail.com>
2025-02-11 09:36:57 +0000

    Added sign in button to Navbar

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,6 +1,7 @@
-import { auth } from "@/auth";
+import { auth, signIn } from "@/auth";
 import Link from "next/link";
 import UserButton from "./user-button";
+import { Button } from "../ui/button";
 
 export default async function NavBar() {
   // TODO: Show the currently logged-in user
@@ -14,8 +15,21 @@ export default async function NavBar() {
         <Link href="/" className="font-bold">
           Next-Auth v5 Tutorial
         </Link>
-        {user ? <UserButton user={user} /> : " "}
+        {user ? <UserButton user={user} /> : <SignInButton />}
       </nav>
     </header>
+  );
+}
+
+function SignInButton() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn();
+      }}
+    >
+      <Button type="submit">Sign in</Button>
+    </form>
   );
 }
