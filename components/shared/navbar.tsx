8711aee31995ad4e52148f579292080193655cc7
NeilRow56 <neilrowland56@gmail.com>
2025-02-11 09:34:07 +0000

    Added navbar with user button

---
+++ components/shared/navbar.tsx
@@ -0,0 +1,21 @@
+import { auth } from "@/auth";
+import Link from "next/link";
+import UserButton from "./user-button";
+
+export default async function NavBar() {
+  // TODO: Show the currently logged-in user
+
+  const session = await auth();
+  const user = session?.user;
+
+  return (
+    <header className="sticky top-0 bg-background px-3 shadow-sm">
+      <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
+        <Link href="/" className="font-bold">
+          Next-Auth v5 Tutorial
+        </Link>
+        {user ? <UserButton user={user} /> : " "}
+      </nav>
+    </header>
+  );
+}
