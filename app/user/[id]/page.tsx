5d07c6f079149145ebfdaf8d5724716be9fd29f3
NeilRow56 <neilrowland56@gmail.com>
2025-02-12 03:25:35 +0000

    Tweaks to user id page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getUserById } from "@/lib/actions/user.actions";
+import { notFound } from "next/navigation";
 
 const UserPage = async (props: {
   params: Promise<{
@@ -7,11 +8,12 @@ const UserPage = async (props: {
 }) => {
   const { id } = await props.params;
   const user = await getUserById(id);
+  if (!user) notFound();
   return (
     <div className="mx-auto max-w-5xl space-y-8">
       <h1 className="h2-bold">{id}</h1>
       <p className="text-muted-foreground">
-        {user?.createdAt.toLocaleDateString()}
+        {user?.createdAt.toLocaleDateString("en-GB")}
       </p>
     </div>
   );
